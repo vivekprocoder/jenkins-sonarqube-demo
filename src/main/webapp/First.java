f1f55eae97db96face0b77d1394bb0761be86d40
vivek yadav <vivek.lpc@gmail.com>
2025-08-10 11:47:46 +0530

    Update First.java

--- src/main/webapp/First.java
+++ src/main/webapp/First.java
@@ -1,7 +1,9 @@
 public class First{
   public static void main(String[] args){
 
-    int var = 42;
+    int var = 42; //unused variable
+
+    String password = "12346"; //Hardcoded password 
 
   }
 }
